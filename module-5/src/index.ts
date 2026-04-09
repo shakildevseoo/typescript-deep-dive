820d358b0e4e2b7860bfe3db5fe7bdb2af671605
shakildevseoo <shakildevseo@gmail.com>
2026-04-09 17:58:40 +0600

    module-5(Extending Interface)

--- module-5/src/index.ts
+++ module-5/src/index.ts
@@ -51,27 +51,132 @@ console.log(user1.joinedAt.toLocaleString())
 
 
 
-// 
+// mixed use (interface and type alias)
 
 type ServiceStatus = "pending" | "ongoing" | "completed" | "canceled"
 
 interface ServiceRequest  {
-    serviceId : number,
-    customerNaeme : string,
-    status : ServiceRequest
+    serviceId : number;
+    customerNaeme : string;
+    status : ServiceRequest;
 }
 
 
 type Student = {
     name : string;
-    fatherName : string
+    fatherName : string;
     age : string;
 }
 
+
+// Mapped Types
 type OptionalStudent = {
    [K in keyof User]? : User[K]
 }
 
 
+
+
+
+
+// Extending Interface
+
+/* মনে করি একটি ই-কমার্স সাইটে আপনার তিন ধরনের ইউজার আছে: **User**, আমার **Seller**, এবং **Admin**।
+
+- সবারই `id`, `name`, এবং `email` আছে।
+- কিন্তু Seller-এর বাড়তি `shopName` আছে।
+- Admin-এর বাড়তি `role` আছে।
+
+এখন আমি কি বারবার `id`, `name`, `email` লিখবো ? কক্ষনোই না! আমি একটি বেস (Base) ইন্টারফেস বানিয়ে তাকে **Extend** করবো । */
+
+
+
+// without extending interface
+interface User {
+    name : string;
+    age : number 
+    email : string
+}
+
+interface Seller {
+    name : string; // repeated
+    age : number; // repeated
+    email : string; // repeated
+   
+}
+
+interface Admin {
+    name : string;// repeated
+    age : number; // repeated
+    email : string;  // repeated
+   
+}
+
+// using
+// base interface
+interface User {
+    name : string;
+    age : number 
+    email : string
+}
+
+interface Seller extends User {
+    ratting : number
+}
+
+interface Admin extends User{
+ role : 'super-admin' | 'moderator'
+}
+
+/* `User` ইন্টারফেসে `email` property **required**।
+
+কিন্তু `Admin` ইন্টারফেসে আমরা চাই:
+
+- সব property inherit হবে
+- শুধু `email` হবে **optional** 
+
+*/
+
+interface Admin  extends Omit<User, "email">{
+    email?: string;
+}
+
+
+
+
+// Multiple Inheritance
+
+interface HasName {
+    name : string
+}
+
+
+interface HasEmail {
+    email : string
+}
+
+
+interface Profile extends HasName, HasEmail{
+    age : number
+}
+
+
+//Intersection of Interfaces
+interface Develoer {
+    name : string;
+    age : number
+}
+
+interface Contact {
+    email : string
+}
+
+type StudentProfile = Develoer & Contact
+
+const student1 : StudentProfile =  {
+    name : "Md Shakil Hossain",
+    age : 20,
+    email : "shakildevseo@gmail.com"
+}
 
 
